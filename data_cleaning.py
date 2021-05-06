b876ab105764f42a4a24bc5027a83ccbf581f925
Amogh Gupta <amoghgupta@berkeley.edu>
2021-05-06 05:19:53 -0700

    functioned

--- data_cleaning.py
+++ data_cleaning.py
@@ -4,19 +4,20 @@ import pandas as pd
 df_capital_loss = {key : capital loss}
 df_age_hours = {key : [age, hours]}
 """
+def getdata(index):
+	df = pd.read_csv("adult.data", header = None)
+	df['key'] = [x for x in range(0, len(df))]
+	df_capital_loss_list = df.iloc[:, [15,11]]
+	df_capital_loss_list.columns = ['key','capital_loss']
+	df_age_hours_list = df.iloc[:,[15,0,12]]
+	df_age_hours_list.columns = ['key','age', 'hours']
+	df_capital_loss = {}
+	df_age_hours = {}
 
-df = pd.read_csv("adult.data", header = None)
-df['key'] = [x for x in range(0, len(df))]
-df_capital_loss_list = df.iloc[:, [15,11]]
-df_capital_loss_list.columns = ['key','capital_loss']
-df_age_hours_list = df.iloc[:,[15,0,12]]
-df_age_hours_list.columns = ['key','age', 'hours']
-df_capital_loss = {}
-df_age_hours = {}
-
-for key in df['key']:
-	df_capital_loss[key] = df_capital_loss_list['capital_loss'][key]
-	df_age_hours[key] = [df_age_hours_list['age'][key], df_age_hours_list['hours'][key]]
+	for key in df['key']:
+		df_capital_loss[key] = df_capital_loss_list['capital_loss'][key]
+		df_age_hours[key] = [df_age_hours_list['age'][key], df_age_hours_list['hours'][key]]
+	return df_capital_loss, df_age_hours
 	
 # print(dict(list(df_capital_loss.items())[0:20]))
 # print(dict(list(df_age_hours.items())[0:20]))
